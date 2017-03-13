067dcf76537202cc7b9b269601a6e7c9ef4636d0
Abel Camacho <cuatrooctavos@yahoo.com.mx>
2017-03-13 07:30:12 +0100

    change colors

--- plot_opinion_matrix.R
+++ plot_opinion_matrix.R
@@ -3,5 +3,5 @@
 plot_opinion<-function(opinions, time){
 N<-sqrt(length(opinions))
 	colmat<-matrix(opinions, nrow=N ,ncol=N)
-  	print(levelplot(colmat,col.regions=c('black','red'),at=c(-1,0,1),ylab='',colorkey=F ,main=list(paste0('Period: ',time),side=1,line=0.5),xlab=''))
+  	print(levelplot(colmat,col.regions=c('#F9CF00','#4484CE'),at=c(-1,0,1),ylab='',colorkey=F ,main=list(paste0('Period: ',time),side=1,line=0.5),xlab=''))
 }
